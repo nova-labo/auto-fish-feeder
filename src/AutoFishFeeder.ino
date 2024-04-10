2286f6a6569811b6466c533f3e288c8c7a379d61
FunnyJay <98242802+nova-labo@users.noreply.github.com>
2024-04-10 22:55:59 +0900

    Add auto feeding feature with status indication

--- src/AutoFishFeeder.ino
+++ src/AutoFishFeeder.ino
@@ -1,40 +1,108 @@
+/*
+  Automatic Fish Feeder
+
+  Automatically feeding fish with a specified time interval, or manually with a push of a button.
+
+  This program should be compatible with most Arduinos out on the market.
+  However, it has only been tested on an Arduino UNO Rev.3 board. LED_BUILTIN is set to the
+  correct LED pin independent of which board is used.
+
+  To make all the magic happen, you will also need the following components:
+
+  + An Arduino UNO (Rev.3) board (preferred, but could be any other Arduino board of your choice)
+  + A SG90 Micro Servo (or something similar)
+  + A Red LED (preferred, but necessary)
+  + A 220 Ohm resistor (LED over current protection)
+  + A momentary push button (for manual control if needed)
+  + some kind of container (to store fish food)
+  + An obsoleted credit card (or any kind of plastic card to be used as latch)
+
+  And of course, you will also need a couple of jump wires to connect all things up! ;-)
+
+  Since the code is pretty much self-explanatory, please feel free to read through and make
+  some tweaks as you wish.
+  Yes, that's right! One push of the button will reset automatic feeding timer.
+
+  Created 10 April 2024
+  By Shunsuke Kawamura
+
+  This code is in the public domain.
+
+  https://github.com/nova-labo/auto-fish-feeder?tab=MIT-1-ov-file
+*/
+
+// ----- Header File Includes ----- //
 #include <Servo.h>
 
-#define BLINK_INTERVAL 1000 // Status LED blink interval
-#define SERVO_TIME 10      // SG90 micro servo operation time per degree
+#define DEBUG
 
-const byte LED_STATUS = 12;   // Status LED
-const byte SERVO_CTRL = 9;    // SG90 micro servo
-const byte PUSH_BUTTON = 2;   // Push button for user input
-const byte CLOSE_ANGLE = 110; // Latch close position angle
-const byte OPEN_ANGLE = 70;   // Latch open position angle
+// ----- Preprocessor Directives ----- //
+#define BLINK_INTERVAL 1000     // [ms] Status led blink interval
+#ifdef DEBUG
+#define FEED_INTERVAL 5000      // [ms] Automatic feed interval for debugging purpose
+#else
+#define FEED_INTERVAL (5 * 24 * 60 * 60 * 1000)      // [ms] Automatic feed interval for normal operation
+#endif
+#define SERVO_TIME 10           // [ms/degree] servo horn movement wait time per degree
 
+// ----- Global Constants ----- //
+const byte SERVO_CTRL = 9;      // SG90 micro servo
+const byte PUSH_BUTTON = 2;     // Push button
+const byte CLOSE_ANGLE = 110;   // Close position angle
+const byte OPEN_ANGLE = 70;     // Open position angle
+#ifdef DEBUG
+const byte LED_STATUS = 12;            // Use external LED for easy debugging
+#else
+const byte LED_STATUS = LED_BUILTIN;   // Use interanl LED for saving electrical energy
+#endif
+
+// ----- Global Variables ----- //
 volatile bool isButtonPushed = false;
+unsigned long currTime = 0;
+unsigned long prevBlinkTime = 0;
+unsigned long prevFeedTime = 0;
 
-unsigned long currentMillis = 0;
-unsigned long previousMillis = 0;
-
-int sg90ServoPos;
+int sg90ServoPos = CLOSE_ANGLE;
 Servo sg90Servo;
 
+/*
+* Hardware setup & configurations to get things ready ;-)
+*/
 void setup() {
-  // Assign pins to I/O
-  pinMode(LED_BUILTIN, OUTPUT);
+  // Configure GPIO pins
   pinMode(LED_STATUS, OUTPUT);
   pinMode(PUSH_BUTTON, INPUT_PULLUP);
 
-  // Assign pin to SG90 micro servo
+  // Configure SG90 micro servo
   sg90Servo.attach(SERVO_CTRL);
   sg90Servo.write(CLOSE_ANGLE);
   delay(SERVO_TIME);
 
-  // Assign pin to push button
+  // Configure push button interrupt
   attachInterrupt(digitalPinToInterrupt(PUSH_BUTTON), buttonPushed, FALLING);
 }
 
+/*
+* Super loop to get it work ;-)
+*/
 void loop() {
+  // Get current time
+  currTime = millis();
+
+  // Blink status led for automatic mode
+  if (!isButtonPushed && currTime - prevBlinkTime >= BLINK_INTERVAL) {
+    prevBlinkTime = currTime;
+    digitalWrite(LED_STATUS, !digitalRead(LED_STATUS));
+  }
+
   // Open and close latch when button pushed manually
-  if (isButtonPushed) {
+  // Servo operation takes longer than 25msec, 
+  // we won't need to worry about signal bouncing caused by the momentary push button.
+  if (isButtonPushed || currTime - prevFeedTime >= FEED_INTERVAL) {
+    prevFeedTime = currTime;
+
+    digitalWrite(LED_STATUS, LOW);  // Blink status led for manual mode
+
     for (sg90ServoPos = CLOSE_ANGLE; sg90ServoPos >= OPEN_ANGLE; sg90ServoPos--) {
       sg90Servo.write(sg90ServoPos);
       delay(SERVO_TIME);
@@ -44,17 +112,15 @@ void loop() {
       sg90Servo.write(sg90ServoPos);
       delay(SERVO_TIME);
     }
-    isButtonPushed = false;
-  }
 
-  // Blink status led
-  currentMillis = millis();
-  if (currentMillis - previousMillis >= BLINK_INTERVAL) {
-    previousMillis = currentMillis;
-    digitalWrite(LED_STATUS, !digitalRead(LED_STATUS));
+    digitalWrite(LED_STATUS, HIGH); // Blink status led for manual mode
+    isButtonPushed = false;
   }
 }
 
+/*
+* Interrupt Service Routine for the push button
+*/
 void buttonPushed() {
   isButtonPushed = true;
 }
